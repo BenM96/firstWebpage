b6d278f39efeba2a39b6f5b5b5a7ef4fb3597c91
BenM96 <49072028+BenM96@users.noreply.github.com>
2019-04-12 11:31:00 +0100

    Add files via upload

---
+++ Recipe/src/main/java/com/qa/persistence/domain/Recipe.java
@@ -0,0 +1,54 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Recipe {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	
+	private String name;
+	private String desc;
+
+	public Recipe() {
+
+	}
+
+	public Recipe(String name, String desc) {
+		this.name = name;
+		this.desc = desc;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+
+
+}
